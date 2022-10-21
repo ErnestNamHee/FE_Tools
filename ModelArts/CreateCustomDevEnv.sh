97dc9905a990f1165a762d90b7356938c9c46329
ErnestNamHee <37908030+ErnestNamHee@users.noreply.github.com>
2022-10-21 18:10:22 +0200

    Create CreateCustomDevEnv.sh

---
+++ ModelArts/CreateCustomDevEnv.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Create env
+Python_Version='3.10.0'
+
+conda create --quiet --yes -n my-env python=$Python_Version
+#source to env
+source /home/ma-user/anaconda3/bin/activate /opt/conda/envs/my-env
+#Mandatory package
+pip install jupyter
+python3 -m ipykernel install --user --name "my-custum-env"
+> /home/ma-user/.local/share/jupyter/kernels/my-custum-env/kernel.json
+cat <<EOT >> /home/ma-user/.local/share/jupyter/kernels/my-custum-env/kernel.json
+{
+ "argv": [
+  "/opt/conda/envs/my-env/bin/python3",
+  "-m",
+  "ipykernel_launcher",
+  "-f",
+  "{connection_file}"
+ ],
+ "display_name": "my-custum-env",
+ "language": "python",
+ "env": {
+        "PATH": "/opt/conda/envs/my-env/bin/:/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/",
+        "http_proxy": "http://proxy-notebook.modelarts.com:8083",
+        "https_proxy": "http://proxy-notebook.modelarts.com:8083",
+        "ftp_proxy": "http://proxy-notebook.modelarts.com:8083",
+        "HTTP_PROXY": "http://proxy-notebook.modelarts.com:8083",
+        "HTTPS_PROXY": "http://proxy-notebook.modelarts.com:8083",
+        "FTP_PROXY": "http://proxy-notebook.modelarts.com:8083"
+        }
+}
+EOT
