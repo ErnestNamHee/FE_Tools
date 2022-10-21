95d765deab936a022ba7ba256784c12992c20604
ErnestNamHee <37908030+ErnestNamHee@users.noreply.github.com>
2022-10-21 18:34:02 +0200

    Update CreateCustomDevEnv.sh

--- ModelArts/CreateCustomDevEnv.sh
+++ ModelArts/CreateCustomDevEnv.sh
@@ -4,7 +4,7 @@
 #### Set Parameters
 Python_Version='3.10.0'
 Conda_Env_Name='ModelArtsEnv01'
-Kernel_Name='Pytorch-Python-3.10'
+Kernel_Name='pytorch-python-3.10'  #Please use small capital only for the Kernel environment name
 
 #### Create Conda Environment
 conda create --quiet --yes -n $Conda_Env_Name python=$Python_Version
